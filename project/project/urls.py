f4c1ae51984322eb61ffacb85fdc72ef37e87e94
MSSohan <sohan.cu.eee.17@gmail.com>
2023-08-13 03:36:04 +0600

    Update urls.py

--- project/project/urls.py
+++ project/project/urls.py
@@ -15,7 +15,11 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from signup.views import signaction
+from login.views import loginaction
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('login/', loginaction),
+    path('signup/', signaction),
 ]
