9d03ea65f88689143eb2c75b23964459853cfd7a
MSSohan <sohan.cu.eee.17@gmail.com>
2023-08-14 23:37:15 +0600

    Update urls.py

--- project/project/urls.py
+++ project/project/urls.py
@@ -24,5 +24,6 @@ urlpatterns = [
     path('', RedirectView.as_view(url='login/', permanent=False), name='default'),
     path('signup/', signaction),
     path('login/', loginaction),
-    path('logout/', auth_views.LogoutView.as_view(), name='logout'),
+    path('logout/', auth_views.LogoutView.as_view(next_page='login/'), name='logout'),
+    path('login/', auth_views.LoginView.as_view(), name='login'),
 ]
