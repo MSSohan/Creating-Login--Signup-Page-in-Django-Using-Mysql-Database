0578ad89f265d43dd2c40ac7128afa4d43c56ed7
MSSohan <sohan.cu.eee.17@gmail.com>
2023-08-16 01:58:26 +0600

    Update views.py

--- project/success/views.py
+++ project/success/views.py
@@ -1,4 +1,6 @@
+from django.contrib.auth.decorators import login_required
 from django.shortcuts import render
 
+@login_required
 def tokenaction(request):
     return render(request, 'success.html')
