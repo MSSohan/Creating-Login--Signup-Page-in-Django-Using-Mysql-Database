ab9b7a9df8ee27730a40b0dd23a6fe8faf065615
MSSohan <sohan.cu.eee.17@gmail.com>
2023-10-01 02:18:31 +0600

    Update models.py

--- project/login/models.py
+++ project/login/models.py
@@ -1,3 +1,12 @@
 from django.db import models
+from django.contrib.auth.models import User
+from django.utils import timezone
+from rest_framework.authtoken.models import Token
 
-# Create your models here.
+class CustomToken(Token):
+    expiration_date = models.DateTimeField()
+
+def update_token_expiration(token):
+    # Set the expiration date to 30 days from now
+    token.expiration_date = timezone.now() + timezone.timedelta(days=30)
+    token.save()
